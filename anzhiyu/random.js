f5b184a614a275605c2d8250dd275c5c86ac9a6f
yuzaoqie <2195150288@qq.com>
2024-04-29 07:47:30 +0000

    更新第一个博客 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/29/hello-world/","2024/04/29/技术随想/"];function toRandomPost(){
+var posts=["2024/04/29/工具分享/GPT搭建/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
