807e9315ca2bb4d9c84ede958f50226300a3016d
yuzaoqie <2195150288@qq.com>
2024-04-29 02:49:32 +0000

    更新博客 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/29/hello-world/"];function toRandomPost(){
+var posts=["2024/04/29/hello-world/","2024/04/29/技术随想/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
