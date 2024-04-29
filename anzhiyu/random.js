2391cf4cfe5444be80725e40642c2d96eac70a6f
yuzaoqie <2195150288@qq.com>
2024-04-29 08:09:01 +0000

    修复图片丢失的bug Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/29/工具分享/GPT搭建/"];function toRandomPost(){
+var posts=["2024/04/29/工具分享/GPT搭建/","2024/04/29/常见问题解决方案/博客的图片丢失问题/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
