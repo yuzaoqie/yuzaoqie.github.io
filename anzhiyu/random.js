59190cb31fdf5305ac8f547ea4a6bbc10da49b99
yuzaoqie <2195150288@qq.com>
2024-05-30 14:37:47 +0000

    1 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/25/AI绘画学习笔记/AI绘画/","2024/04/29/工具分享/GPT搭建/","2024/05/22/杂谈/SSM/"];function toRandomPost(){
+var posts=["2024/05/25/AI绘画学习笔记/AI绘画/","2024/05/25/AI绘画学习笔记/AI绘画常用模型/","2024/04/29/工具分享/GPT搭建/","2024/05/22/杂谈/SSM/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
