253c6af6cb9c8e7efb60b0f8f4c94f2e165c949c
yuzaoqie <2195150288@qq.com>
2026-04-17 09:45:44 +0000

    更新文章 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/05/25/AI绘画学习笔记/AI绘画/","2024/05/29/AI绘画学习笔记/AI绘画常用模型/","2024/06/02/AI绘画学习笔记/ComfyUI工作流AI绘画/","2024/04/29/工具分享/GPT搭建/","2024/06/02/工具分享/突破网盘下载速度限制/"];function toRandomPost(){
+var posts=["2026/04/18/HermesAgent/Hermes-Agent安装与快速上手教程/","2026/04/19/OpenclawTavilySearch/Openclaw集成TavilySearch/","2026/04/17/PlaywrightCLI/Playwright-CLI集成指南/","2024/04/29/工具分享/GPT搭建/","2024/06/02/工具分享/突破网盘下载速度限制/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
