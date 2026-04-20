5f3d385452f813f2c9e7b5f605489e7260db78dd
yuzaoqie <2195150288@qq.com>
2026-04-20 07:28:20 +0000

    新增文章：我以后的博客是写给 AI 看的 Updated By Github Actions

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/04/18/HermesAgent/Hermes-Agent安装与快速上手教程/","2026/04/19/OpenclawTavilySearch/Openclaw集成TavilySearch/","2026/04/17/PlaywrightCLI/Playwright-CLI集成指南/","2024/04/29/工具分享/GPT搭建/","2024/06/02/工具分享/突破网盘下载速度限制/"];function toRandomPost(){
+var posts=["2026/04/20/AI博客写作/AI博客写作/","2026/04/18/HermesAgent/Hermes-Agent安装与快速上手教程/","2026/04/19/OpenclawTavilySearch/Openclaw集成TavilySearch/","2024/04/29/工具分享/GPT搭建/","2024/06/02/工具分享/突破网盘下载速度限制/","2026/04/17/PlaywrightCLI/Playwright-CLI集成指南/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
